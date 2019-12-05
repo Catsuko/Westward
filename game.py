e73928f8675c38d0e9c3badd01a7666329ee4bef
Lewis Reid <lreidwilliams@gmail.com>
2019-12-05 20:41:26 +1000

    improves console view so areas are printed as a grid

--- game.py
+++ game.py
@@ -4,13 +4,9 @@ from spaces.blocked_space import BlockedSpace
 from spaces.tile import Tile
 from views.console_view import ConsoleView
 
-# TODO: AreaBuilder
-# TODO: Actors and ActorEngines
-# TODO: ConsoleView printing in a grid structure
-
 open_space = OpenSpace()
 area = Area([
-    Tile(0, 0, open_space), Tile(1, 0, open_space),
-    Tile(0, 1, open_space), Tile(1, 1, BlockedSpace())
+    Tile(0, 0, open_space), Tile(1, 0, open_space), Tile(2, 0, open_space),
+    Tile(0, 1, open_space), Tile(1, 1, open_space), Tile(2, 1, open_space)
 ])
 area.print_to(ConsoleView())
--- views/console_view.py
+++ views/console_view.py
@@ -3,7 +3,7 @@ from functools import reduce
 
 class ConsoleView:
 
-    def __init__(self, contents=[]):
+    def __init__(self, contents=""):
         self.contents = contents
 
     def print_area(self, tiles):
@@ -18,7 +18,13 @@ class ConsoleView:
         return self.with_character(x, y, "#")
 
     def with_character(self, x, y, character):
-        return ConsoleView(self.contents + [character])
+        rows = self.contents.split("\n")
+        width = len(rows[0])
+        height = len(rows)
+        rows.extend([" " for i in range(max((y-height)+1, 0))])
+        rows = [row + (" " * max(x - width, 0)) for row in rows]
+        rows[y] = rows[y][:x] + character + rows[y][x+1:]
+        return ConsoleView("\n".join(rows))
 
     def __str__(self):
-        return ''.join(self.contents)
+        return self.contents
