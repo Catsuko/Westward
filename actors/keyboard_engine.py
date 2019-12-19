c73edbc04b3012b6731ea6aa4cb300f63e38fa8a
Lewis Reid <lreidwilliams@gmail.com>
2019-12-19 21:57:34 +1000

    reworked keyboard engine so update time is constant

--- actors/keyboard_engine.py
+++ actors/keyboard_engine.py
@@ -4,16 +4,18 @@ import time
 
 class KeyboardEngine:
 
-    def __init__(self, key_map, timeout=1):
+    def __init__(self, key_map, timeout=0.75, time_step=0.1):
         self.key_map = key_map
         self.timeout = timeout
+        self.time_step = time_step
 
     def pick_movement_direction(self, origin, world):
-        start = time.time()
         key_pressed = None
-        while key_pressed is None and time.time() - start < self.timeout:
-            time.sleep(0.001)
+        remaining_timeout = self.timeout
+        time_step = remaining_timeout * max(1.0, self.time_step)
+        while key_pressed is None and remaining_timeout > 0:
+            time.sleep(time_step)
+            remaining_timeout = remaining_timeout - time_step
             key_pressed = next((key for key in self.key_map.keys() if keyboard.is_pressed(key)), None)
-        move_direction = (0,0) if key_pressed is None else self.key_map[key_pressed]
-        time.sleep(0.2)
-        return move_direction
+        time.sleep(remaining_timeout)
+        return (0,0) if key_pressed is None else self.key_map[key_pressed]
