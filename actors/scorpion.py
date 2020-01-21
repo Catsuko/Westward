a70a2437e3c76b0d44b564bdd5db491f115cd795
Lewis Reid <lreidwilliams@gmail.com>
2020-01-21 21:40:39 +1000

    scorpion moves at half speed

--- actors/scorpion.py
+++ actors/scorpion.py
@@ -1,15 +1,19 @@
 class Scorpion:
 
-    def __init__(self, target, movement):
+    def __init__(self, target, movement, waiting=True):
         self.target = target
         self.movement = movement
+        self.waiting = waiting
 
     def act(self, tile, root):
-        updated_target = self.target.with_area(root)
-        target_direction = updated_target.direction_to(tile)
-        if target_direction[0] is not 0 and target_direction[1] is not 0:
-            target_direction = (target_direction[0], 0)
-        return self.movement.redirect(*target_direction).on(self, tile, root)
+        if self.waiting:
+            return tile.enter(self.__alternate_wait(), tile, root)
+        else:
+            updated_target = self.target.with_area(root)
+            target_direction = updated_target.direction_to(tile)
+            if target_direction[0] is not 0 and target_direction[1] is not 0:
+                target_direction = (target_direction[0], 0)
+            return self.movement.redirect(*target_direction).on(self.__alternate_wait(), tile, root)
 
     def interact_with(self, actor, origin, tile, root):
         return root.with_tile(origin)
@@ -22,6 +26,9 @@ class Scorpion:
 
     def matches(self, other_actor):
         return other_actor.identifies_with("s")
+
+    def __alternate_wait(self):
+        return Scorpion(self.target, self.movement, not self.waiting)
 
     def __str__(self):
         return "Scorpion"
