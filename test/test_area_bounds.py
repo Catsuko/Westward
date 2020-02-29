163f0a0563a14b7c856e19e2efb6515abf3ee9fe
Lewis Reid <lreidwilliams@gmail.com>
2020-02-29 18:38:05 +1000

    added test coverage for area and tile equality

--- test/test_area_bounds.py
+++ test/test_area_bounds.py
@@ -2,6 +2,7 @@ import unittest
 
 from world.area import Area
 from world.area_builder import AreaBuilder
+from world.blocked_space import BlockedSpace
 from world.open_space import OpenSpace
 from world.tile import Tile
 
@@ -54,6 +55,42 @@ class AreaBoundsTests(unittest.TestCase):
         tile = Tile(2, 2, OpenSpace())
         area = AreaBuilder().rectangle(10, 10).to_area()
         self.assertFalse(area.enclosed_by(tile))
+
+    def test_areas_of_same_size_and_position_are_equal(self):
+        builder = AreaBuilder().rectangle(5, 5)
+        area_a = builder.to_area()
+        area_b = builder.to_area()
+        self.assertEqual(area_a, area_b)
+
+    def test_areas_of_same_size_but_different_position_are_not_equal(self):
+        area_a = AreaBuilder().rectangle(5, 5).to_area()
+        area_b = AreaBuilder().reposition(100, 0).rectangle(5, 5).to_area()
+        self.assertNotEqual(area_a, area_b)
+
+    def test_areas_of_different_size_are_not_equal(self):
+        area_a = AreaBuilder().rectangle(5, 1)
+        area_b = AreaBuilder().rectangle(1, 5)
+        self.assertNotEqual(area_a, area_b)
+
+    def test_tiles_at_the_same_position_are_equal(self):
+        tile_a = Tile(5, 5, OpenSpace())
+        tile_b = Tile(5, 5, BlockedSpace())
+        self.assertEqual(tile_a, tile_b)
+
+    def test_tiles_at_different_positions_are_not_equal(self):
+        tile_a = Tile(1, 5, OpenSpace())
+        tile_b = Tile(5, 1, BlockedSpace())
+        self.assertNotEqual(tile_a, tile_b)
+
+    def test_tile_is_not_equal_to_surround_area(self):
+        area = AreaBuilder().rectangle(5, 5).to_area()
+        tile = area.tile(2, 2)
+        self.assertNotEqual(tile, area)
+
+    def test_surround_area_is_not_equal_to_tile(self):
+        area = AreaBuilder().rectangle(5, 5).to_area()
+        tile = area.tile(2, 2)
+        self.assertNotEqual(area, tile)
 
 
 if __name__ == '__main__':
